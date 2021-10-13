c15679b1ec0b0868761b847fc9fa129a268b9d3e
Victor Modesto <vmodesto2002@gmail.com>
2021-10-13 14:31:38 -0300

    edited settings

--- app/integrations/soccer_api/championship.rb
+++ app/integrations/soccer_api/championship.rb
@@ -1,11 +1,10 @@
 module SoccerApi
   class Championship
     URL = 'https://api.api-futebol.com.br/v1/campeonatos'
-    TOKEN = 'Bearer live_cd4a5d73e3dce3958d01b49ac88bf7'
     PARAMS = {:sections => 42336}
     def get_championships
       response = Faraday.get URL, PARAMS do |request|
-        request.headers["Authorization"] = TOKEN
+        request.headers["Authorization"] = $api_key
       end
       championships = JSON.parse response.body
       return championships
